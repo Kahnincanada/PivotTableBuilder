83f527229ba6eb056feb2bff2330f5e4d8756320
Tony Pang <tong.pang@mail.mcgill.ca>
2017-02-01 16:01:09 -0500

    test

--- TuesdayProject/Functions/PivotTable.java
+++ TuesdayProject/Functions/PivotTable.java
@@ -2,7 +2,11 @@ package TuesdayProject.Functions;
 
 import TuesdayProject.Loaders.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 /**
@@ -14,17 +18,38 @@ public class PivotTable {
     private HashMap<Integer,HashMap<Integer, Double>> userListRatingData;
     private ArrayList<HashMap<Integer,Double>> movieListRatingData;
     private ArrayList<String> tagInfo;
+    private ArrayList<String> schemaChoice;
+    private String[] fields = new String[] {"actor", "director", "country", "genre", "location", "movietag", "tag", "userrating", "movie"};
 
     public PivotTable(){
         rawData= new ArrayListMatrix();
         userListRatingData = new HashMap<>();
         tagInfo = new ArrayList<>();
+        ChooseSchema();
     }
     public PivotTable(ArrayList<String> fields){
         rawData= new ArrayListMatrix();
         userListRatingData = new HashMap<>();
         tagInfo = new ArrayList<>();
         Load(fields);
+    }
+    public void ChooseSchema(){
+        //This allows not loading all movies all the times
+        //need to add fail proof tests
+
+
+
+
+
+
+        //
+        System.out.println("Available field for pivot table are : "+ Arrays.toString(fields));
+        System.out.println("Please enter the field for pivot table schema:");
+        schemaChoice = ReadInput();
+        System.out.println(schemaChoice.toString());
+        Load(schemaChoice);
+
+
     }
     public void Load(ArrayList<String> fields){
         if(fields.contains("tag")) {
@@ -43,11 +68,30 @@ public class PivotTable {
         Loader l = new MovieLoader();
         l.Load(fields);
         rawData = l.getImportedRawData();
+        tagInfo = l.getTagInfo();
     }
 
-
-
-
+    private ArrayList<String> ReadInput(){
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        ArrayList<String> line = new ArrayList<>();
+        try {
+            for(String item : br.readLine().split(",")) line.add(item);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        /*try {
+            isr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+        return line;
+    }
 
     public static void Filter(){
 
@@ -63,5 +107,7 @@ public class PivotTable {
 
     public ArrayList<HashMap<Integer, Double>> getMovieListRatingData() {return movieListRatingData;}
 
-    public ArrayList<String> getTagInfo() {return tagInfo;}
+    public ArrayList<String> getTagInfo() {
+        return tagInfo;
+    }
 }
--- TuesdayProject/Main.java
+++ TuesdayProject/Main.java
@@ -1,24 +1,12 @@
 package TuesdayProject;
 
-import TuesdayProject.UnitTests.*;
-import TuesdayProject.Functions.*;
-import TuesdayProject.Loaders.*;
-import javafx.application.Platform;
-import javafx.embed.swing.JFXPanel;
-import javafx.scene.Scene;
-import javafx.scene.web.WebView;;
+import TuesdayProject.UnitTests.PivotTableTests;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        LoaderTests lt = new LoaderTests();
-        ArrayListMatrix mat = new ArrayListMatrix();
-        mat = lt.getAlm();
-        int i = mat.Count("actorName","berg");
-        System.out.println(mat.get(1).get("actorName"));
-        //System.out.println(mat.getField());
-        System.out.println(i);
+        PivotTableTests ptv = new PivotTableTests();
+        ptv.ChooseSchemaTest();
     }
 }
